ac81098419be4033eb9550ef721a29ed84177a8f
c0rv0s <inathan.m@gmail.com>
2016-04-20 22:38:58 -0700

    small UI fixes

---
+++ Knot/interests.swift
@@ -0,0 +1,21 @@
+//
+//  interests.swift
+//  Knot
+//
+//  Created by Nathan Mueller on 4/20/16.
+//  Copyright © 2016 Knot App. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Interests: UIViewController {
+    
+    var interests: Array<String>!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
