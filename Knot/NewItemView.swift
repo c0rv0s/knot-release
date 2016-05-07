3c8839bd62ce6da2ce7c6080f799fa02bba61da3
c0rv0s <inathan.m@gmail.com>
2016-05-07 00:27:04 -0700

    test code was activated, removed for this commit and push.

--- Knot/NewItemView.swift
+++ Knot/NewItemView.swift
@@ -810,6 +810,7 @@ UIPickerViewDataSource, UIPickerViewDelegate, UIImagePickerControllerDelegate, U
     //end upload and submissions
     @IBAction func cancelListing(sender: AnyObject) {
         
+        /*
         //some auth testing code
         self.appDelegate.item = ListItem()
         
@@ -832,7 +833,7 @@ UIPickerViewDataSource, UIPickerViewDelegate, UIImagePickerControllerDelegate, U
         self.presentViewController(vc, animated: true, completion: nil)
         //end test code
         
-        /*
+        */
 
         if nameField.text == "Booty" {
             if let url = NSURL(string: "http://i.imgur.com/JpolRoC.gif") {
@@ -874,7 +875,7 @@ UIPickerViewDataSource, UIPickerViewDelegate, UIImagePickerControllerDelegate, U
 
         let vc = self.storyboard!.instantiateViewControllerWithIdentifier("Reveal View Controller") as! UIViewController
         self.presentViewController(vc, animated: true, completion: nil)
- */
+ 
  
     }
     
