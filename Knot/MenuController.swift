460486128944d30fb665d7e69ec7b27a0b63e459
c0rv0s <inathan.m@gmail.com>
2016-05-14 22:33:37 -0700

    UI fix for menuscene

--- Knot/MenuController.swift
+++ Knot/MenuController.swift
@@ -31,6 +31,8 @@ class MenuController: UITableViewController {
         
         //selfRatingArray = []
         
+        self.view.backgroundColor = normalColor
+        
         self.floatRatingView.emptyImage = UIImage(named: "empty-star")
         self.floatRatingView.fullImage = UIImage(named: "full-star")
         self.floatRatingView.editable = false
@@ -47,6 +49,10 @@ class MenuController: UITableViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return .LightContent
     }
     
     func returnUserDataForProf() {
--- Knot/SWRevealViewController.m
+++ Knot/SWRevealViewController.m
@@ -81,6 +81,10 @@ static CGFloat scaledValue( CGFloat v1, CGFloat min2, CGFloat max2, CGFloat min1
     return result;
 }
 
+- (UIStatusBarStyle) preferredStatusBarStyle {
+    return UIStatusBarStyleLightContent;
+}
+
 
 - (id)initWithFrame:(CGRect)frame controller:(SWRevealViewController*)controller
 {
